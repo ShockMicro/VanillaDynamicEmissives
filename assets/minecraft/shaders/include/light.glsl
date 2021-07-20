7039955c1ae840d5d224e7aeb289a2be5639614a
ShockMicro <56613693+ShockMicro@users.noreply.github.com>
2021-07-20 15:15:29 -0400

    Add missing alpha change

--- assets/minecraft/shaders/include/light.glsl
+++ assets/minecraft/shaders/include/light.glsl
@@ -39,5 +39,6 @@ vec4 make_emissive(vec4 inputColor, vec4 lightColor, float vertexDistance, float
 float remap_alpha(float inputAlpha) {
 	if (check_alpha(inputAlpha, 252.0) == 1.0) return 255.0; // Default case, checks for alpha 252 and converts all pixels of that to alpha 255.
 	else if (check_alpha(inputAlpha, 251.0) == 1.0) return 190.0; // Copypaste this and change the numbers to add a custom alpha value.
+	else if (check_alpha(inputAlpha, 250.0) == 1.0) return 255.0; // Same as above
 	else return inputAlpha; // Fallback case, if none of the pixels are meant to be mapped then 
 }
