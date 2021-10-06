37e297105352e01667e9cf6ab1fdbcbff47ce12a
ShockMicro <56613693+ShockMicro@users.noreply.github.com>
2021-10-06 18:51:32 -0400

    Fix mipmapping bug, change examples

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Allows for dynamic emissive textures in vanilla Minecraft.
 
 ### How to Use
-If you want to make a solid texture emissive, just set the alpha value to 252 in your favorite image editor. You can see an example of this with diamond ore. 
+If you want to make a solid texture emissive, just set the alpha value to 252 in your favorite image editor. You can see an example of this in the redstone ore texture. 
 
 Translucent textures are a bit more tricky, but it's still fairly simple. Go to the `light.glsl` file and open it up. There are two functions you want 
 to look at there, the ones that tell you to copy/paste. If it's inside a `check_alpha()` function, that's the alpha value you're checking for. If it's right 
--- assets/minecraft/shaders/include/emissive_utils.glsl
+++ assets/minecraft/shaders/include/emissive_utils.glsl
@@ -9,18 +9,12 @@ float check_alpha(float textureAlpha, float targetAlpha) {
 
 vec4 make_emissive(vec4 inputColor, vec4 lightColor, float vertexDistance, float inputAlpha) {
 	if (check_alpha(inputAlpha, 252.0) == 1.0) return inputColor; // Default case, checks for alpha 252 and just returns the input color if it is.
-	
-	// Examples
 	else if (check_alpha(inputAlpha, 251.0) == 1.0) return inputColor; // Copypaste this and change the number to add a custom alpha value.
-	else if (check_alpha(inputAlpha, 250.0) == 1.0 && vertexDistance < 8.0) return inputColor; // Checks for alpha 250 and if it's near the player.
-	else if (check_alpha(inputAlpha, 250.0) == 1.0 && lightColor.r > 0.4) return inputColor; // Checks for alpha 250 and if it's above a certain light color, which is about light level 10.
-	
-	else return inputColor * lightColor; // Fallback case, if none of the pixels are supposed to be emissive, then it adds the light.
+	else return inputColor * lightColor; // If none of the pixels are supposed to be emissive, then it adds the light.
 }
 
 float remap_alpha(float inputAlpha) {
 	if (check_alpha(inputAlpha, 252.0) == 1.0) return 255.0; // Default case, checks for alpha 252 and converts all pixels of that to alpha 255.
 	else if (check_alpha(inputAlpha, 251.0) == 1.0) return 190.0; // Copypaste this and change the numbers to add a custom alpha value.
-	else if (check_alpha(inputAlpha, 250.0) == 1.0) return 255.0; // Same as above
-	else return inputAlpha; // Fallback case, if none of the pixels are meant to be mapped then 
+	else return inputAlpha; // If none of the pixels are meant to be mapped then it just doesn't map.
 }
